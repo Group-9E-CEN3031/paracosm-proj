37405669e8ba78da32ba00b81bd773e3a1a6a6f5
Waddy Leonvil <waddyleonvil@ufl.edu>
2019-12-02 19:33:51 -0500

    Sprint 2 change

--- client/src/App.js
+++ client/src/App.js
@@ -1,83 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import Component from 'react';
+import { Route, Switch, Redirect  } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from "./views/Home/Home"
+import Register from"./views/Register/Register"
+import Upload from "./views/FileUpload/Upload"
+import NotFound from "./views/NotFound"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <div id="topBar">
-        <p className="topText" id="topParacosm">
-          Paracosm
-        </p>
-        <p className="topText" id="upload">
-          Upload
-        </p>
-        <p className="topText" id="view">
-          View
-        </p>
-
-      </div>
-      <div id="midBar">
-        <table id="midTable">
-          <tr id="fileRow">
-            <p id="files">
-              The Files
-            </p>
-          </tr>
-          <tr>
-            <input type="text" placeholder="UUID Text Field" />
-          </tr>
-        </table>
-
-        <div id="subDiv">
-          <button id="subButton">
-            Submit
-          </button>
-        </div>
-      </div>
-      <header className="App-header">
-        <div className="pillar" id="leftDiv">
-          <div className="subtitle">
-            Image
-          </div>
-          <p>
-            File and function description
-          </p>
-
-          <input type="file" name="file" id="real-file" hidden="hidden" onChange={(e)=>this.onChange(e)}/>
-          <button type="button" id="testButton" className="fileButton">
-            Choose a file...
-          </button>
-        </div>
-
-        <div className="pillar" id="centerDiv">
-          <div className="subtitle">
-            YML
-          </div>
-          <p>
-            File and function description
-          </p>
-          <button className="fileButton">
-            Choose a file...
-          </button>
-        </div>
-
-        <div className="pillar" id="rightDiv">
-          <div className="subtitle">
-            ROS
-          </div>
-          <p>
-            File and function description
-          </p>
-          <button className="fileButton">
-            Choose a file...
-          </button>
-        </div>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path="/Home" component={Home} />
+        <Route exact path="/Register" component ={Register}/>
+        <Route exact path="/Upload" component={Upload} />
+        <Route exact path="/">
+          <Redirect to="/Upload"/>
+        </Route>
+        <Route component={NotFound}/>
+      </Switch>
     </div>
   );
 }
-
-
 
 export default App;
